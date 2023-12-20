950060842d171f46f31471f1db3c4b1445313e51
seyeonsim <seyeonsimw@gmail.com>
2023-12-20 22:58:43 +0900

    검색/ 검색 결과 상세

--- routes/index.js
+++ routes/index.js
@@ -5,5 +5,8 @@ const controller = require('../controller/Cmain')
 router.get('/', controller.main);
 router.get('/login', controller.signin);
 router.get('/signup', controller.signup);
+router.get('/search', controller.search);
+router.get('/searchDetail', controller.searchDetail);
+
 
 module.exports = router;
