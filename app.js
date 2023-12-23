bd9dcd9a6781fe15635e739ba62a212dae4a2f0e
KwonRam <kms971226@gmail.com>
2023-12-23 11:27:40 +0900

    상세페이지 합침

--- app.js
+++ app.js
@@ -35,6 +35,9 @@ app.use(express.json());
 const indexRouter = require('./routes/index');
 app.use('/', indexRouter);
 
+const bookShelfRouter = require('./routes/bookMain');
+app.use('/bookShelf', bookShelfRouter);
+
 // 프로필 사진 업로드
 // app.post('/upload', uploadDetail.single('userfile'), (req, res)=>{
 //     console.log(req.file);
