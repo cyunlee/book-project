b0ee2ffe4d7bebbef9966be5a3727674fe6e618d
Hanwoori00 <729han@gmail.com>
2023-12-28 16:36:27 +0900

    other

--- static/js/otherpage.js
+++ static/js/otherpage.js
@@ -1,16 +1,41 @@
 const followBtn = document.querySelector('.follow');
 const unfollowBtn = document.querySelector('.unfollow');
 
-function follow() {
-    followBtn.addEventListener('click', () => {
+async function follow() {
+	followBtn.addEventListener('click', () => {
         followBtn.classList.add('hidden');
         unfollowBtn.classList.remove('hidden');
     });
+	const otherUser = document.querySelector('#id').value;
+	otherUser = otherUser.substr(1)
+	console.log('idCheck>>>@@@@>>', otherUser);
+	try {
+		await axios({
+			method: "POST",
+			url: "/follow",
+			data: {
+				followingId: otherUser
+			}
+		});	} catch (error) {
+		console.log('interval error : ',error);
+	}
 }
 
-function unfollow() {
-    unfollowBtn.addEventListener('click', () => {
+async function unfollow() {
+	unfollowBtn.addEventListener('click', () => {
         followBtn.classList.remove('hidden');
         unfollowBtn.classList.add('hidden');
     });
+	const otherUser = document.querySelector('#follow').value;
+	try {
+		await axios({
+			method: "POST",
+			url: "/unfollow",
+			data: {
+				followingId: otherUser
+			}
+		});
+	} catch (error) {
+		console.log('interval error : ',error);
+	}
 }
