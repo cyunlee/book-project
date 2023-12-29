a9bc4b97885d53452561327943a0480c25ceacb0
seyeonsim <seyeonsimw@gmail.com>
2023-12-30 01:04:21 +0900

    port

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 8001;
+const PORT = 8000;
 const db = require('./models/index');
 const multer = require('multer');
 const path = require('path');
